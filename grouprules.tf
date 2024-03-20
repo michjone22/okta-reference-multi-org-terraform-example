772e1796ec7090cd9351d9cc87f0d9bbc7f69388
Michael P Jones <8367892+michjone22@users.noreply.github.com>
2024-03-20 15:31:01 -0400

    Update grouprules.tf

--- grouprules.tf
+++ grouprules.tf
@@ -10,6 +10,6 @@ resource "okta_group_rule" "test_rule" {
   name = "Test Rule" 
   status = "ACTIVE" 
   group_assignments = ["${okta_group.test_group.id}"] 
-  expression_type = "urn:okta:expression:1.0" 
-  expression_value = "String.startsWith(user.firstName,"somename")" 
+  expression_type = "urn:okta:expression:1.0"
+  expression_value = "String.startsWith(user.firstName,\"somename\")"
 }
